f9e0789f3fec989d46133ae9ee1571193fbb7fa2
Maximilian Schneider <maximilian.schneider@hpi.uni-potsdam.de>
2011-01-28 01:11:40 +0100

    finished push_back benchmark

--- benchmark/push_back.cc
+++ benchmark/push_back.cc
@@ -1,5 +1,5 @@
 #include "benchmark/benchmark.h"
-#include "varbit/vector.h"
+#include "include/vector.h"
 
 template <typename vector_type>
 uint64_t PushBack(vector_type* vector) {
@@ -10,7 +10,7 @@ uint64_t PushBack(vector_type* vector) {
 }
 
 template <typename vector_type>
-void SumUsingSTL(const char* vector_name, const int max_bit_width) {
+void PushBackUsingSTL(const char* vector_name, const int max_bit_width) {
   Benchmark<vector_type*, uint64_t> benchmark_reserved(
       PushBack<vector_type>,
       vector_name,
@@ -32,7 +32,7 @@ void SumUsingSTL(const char* vector_name, const int max_bit_width) {
 }
 
 template <typename vector_type>
-void SumUsingVarbit(const char* vector_name) {
+void PushBackUsingVarbit(const char* vector_name) {
   Benchmark<vector_type*, uint64_t> benchmark_reserved(
       PushBack<vector_type>,
       vector_name,
@@ -41,7 +41,7 @@ void SumUsingVarbit(const char* vector_name) {
       PushBack<vector_type>,
       vector_name,
       "PushBackUnreserved");
-  for (int bit_width = 1; bit_width <= max_bit_width; ++bit_width) {
+  for (int bit_width = 1; bit_width <= vector_type::max_bit_width(); ++bit_width) {
     vector_type reserved_vector(bit_width, Config().num_elements());
     vector_type unreserved_vector(bit_width);
     volatile uint64_t result_reserved = benchmark_reserved.run(&reserved_vector,
@@ -55,13 +55,13 @@ void SumUsingVarbit(const char* vector_name) {
 
 int main(int argc, char** argv) {
   Configuration config = Config(argc, argv);
-  SumUsingVarbit<varbit::vector<uint8_t> >("varbit8");
-  SumUsingVarbit<varbit::vector<uint16_t> >("varbit16");
-  SumUsingVarbit<varbit::vector<uint32_t> >("varbit32");
-  SumUsingVarbit<varbit::vector<uint64_t> >("varbit64");
-  SumUsingSTL<std::vector<uint8_t> >("std8", 8);
-  SumUsingSTL<std::vector<uint16_t> >("std16", 16);
-  SumUsingSTL<std::vector<uint32_t> >("std32", 32);
-  SumUsingSTL<std::vector<uint64_t> >("std64", 64);
+  PushBackUsingVarbit<varbit::vector<uint8_t> >("varbit8");
+  PushBackUsingVarbit<varbit::vector<uint16_t> >("varbit16");
+  PushBackUsingVarbit<varbit::vector<uint32_t> >("varbit32");
+  PushBackUsingVarbit<varbit::vector<uint64_t> >("varbit64");
+  PushBackUsingSTL<std::vector<uint8_t> >("std8", 8);
+  PushBackUsingSTL<std::vector<uint16_t> >("std16", 16);
+  PushBackUsingSTL<std::vector<uint32_t> >("std32", 32);
+  PushBackUsingSTL<std::vector<uint64_t> >("std64", 64);
   return 0;
 }
