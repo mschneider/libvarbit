79a7d46dff7f26942744218ad45e2b87c7732a42
Maximilian Schneider <maximilian.schneider@hpi.uni-potsdam.de>
2011-02-01 17:53:12 +0100

    refactored papi integration into benchmark

--- benchmark/benchmark.h
+++ benchmark/benchmark.h
@@ -10,9 +10,12 @@
 #ifndef PAPI_COUNTERS
 #define PAPI_COUNTERS {PAPI_TOT_CYC, PAPI_TOT_INS}
 #endif  // PAPI_COUNTERS
-#ifndef PAPI_NUM_COUNTERS
-#define PAPI_NUM_COUNTERS 5
-#endif  // PAPI_NUM_COUNTERS
+#ifndef NUM_PAPI_COUNTERS 
+#define NUM_PAPI_COUNTERS 2 
+#endif  // NUM_PAPI_COUNTERS 
+const int num_papi_counters = NUM_PAPI_COUNTERS;
+// Must not be const, because PAPI's interface is weird.
+int papi_counters[NUM_PAPI_COUNTERS] = PAPI_COUNTERS;
 
 // Simple Configuration class handling command lines parameters and benchmark
 // global setup. Aways create an instance of this in your main:
@@ -22,8 +25,6 @@ class Configuration {
  public:
   Configuration(int argc, char **argv);
   uint64_t num_elements() const { return num_elements_; }
-  unsigned int num_papi_counters() const { return PAPI_NUM_COUNTERS; }
-  int64_t* papi_counters() const { return PAPI_COUNTERS; }
  private:
   uint64_t num_elements_;
 };
@@ -33,7 +34,7 @@ Configuration::Configuration(int argc, char **argv) {
     std::cerr << "mlockall failed: Couldn't lock memory." << std::endl;
     exit(1);
   }
-  const int status = PAPI_start_counters(papi_counters(), num_papi_counters());
+  const int status = PAPI_start_counters(papi_counters, num_papi_counters);
   if (status != PAPI_OK) {
     std::cout << "Can't start PAPI Counters (" << status << ")." << std::endl;
     exit(1);
@@ -65,7 +66,7 @@ class Benchmark {
   Result(*function_)(Input);
   const char *function_name_;
   const char *data_structure_name_;
-  int64_t *papi_values_;
+  long long int papi_values_[num_papi_counters]; // NOLINT PAPI interface again
 };
 
 template<typename Input, typename Result>
@@ -74,12 +75,11 @@ Benchmark<Input, Result>::Benchmark(Result(*function)(Input),
                                     const char *function_name)
     : function_(function),
       function_name_(function_name),
-      data_structure_name_(data_structure_name),
-      papi_values_(new int64_t[Config().num_papi_counters()]) {
+      data_structure_name_(data_structure_name) {
     std::cout << "#function,data structure,bit width";
-    for (unsigned int i = 0; i < Config().num_papi_counters(); ++i) {
+    for (int i = 0; i < num_papi_counters; ++i) {
       char event_name[PAPI_MAX_STR_LEN];
-      PAPI_event_code_to_name(Config().papi_counters(), event_name);
+      PAPI_event_code_to_name(papi_counters[i], event_name);
       std::cout << ',' << event_name;
     }
     std::cout << std::endl;
@@ -87,13 +87,13 @@ Benchmark<Input, Result>::Benchmark(Result(*function)(Input),
 
 template<typename Input, typename Result>
 Result Benchmark<Input, Result>::run(Input input, const int bit_width) {
-  PAPI_read_counters(papi_values_, PAPI_NUM_COUNTERS);
+  PAPI_read_counters(papi_values_, num_papi_counters);
   Result result = function_(input);
-  PAPI_read_counters(papi_values_, PAPI_NUM_COUNTERS);
+  PAPI_read_counters(papi_values_, num_papi_counters);
   std::cout << function_name_ << ',' << data_structure_name_ << ','
             << bit_width;
-  for (unsigned int i = 0; i < Config().num_papi_counters(); ++i) {
-    std::cout << ',' << papi_values_[i]);
+  for (int i = 0; i < num_papi_counters; ++i) {
+    std::cout << ',' << papi_values_[i];
   }
   std::cout << std::endl;
   return result;
